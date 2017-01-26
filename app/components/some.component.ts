7e1080fb74df7f8f83dc2b1cf1e344d06c1f9482
Anubhav Singh <Anubhav Singh>
2017-01-26 18:17:16 +0000

    added css

--- app/components/some.component.ts
+++ app/components/some.component.ts
@@ -4,5 +4,6 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'some',
   templateUrl: `some.component.html`,
+  styleUrls: ['some.component.css']
 })
 export class SomeComponent  { name = 'SomeComponent'; }
